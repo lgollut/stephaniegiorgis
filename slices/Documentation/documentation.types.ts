83348da3b3f6a052c621a11f05e51fd8dee74452
Louis Gollut <louis.gollut@gmail.com>
2023-10-09 20:54:21 +0200

    Remove type definition leftover

--- slices/Documentation/documentation.types.ts
+++ slices/Documentation/documentation.types.ts
@@ -1,7 +1,4 @@
 import { SliceComponentProps } from '@prismicio/react';
-import { RecipeVariants } from '@vanilla-extract/recipes';
-
-import { documentationWrapper } from '@/slices/Documentation/documentation-image-wrapper.css';
 
 import type { Content } from '@prismicio/client';
 
@@ -11,7 +8,3 @@ export type DocumentationProps =
 export const availableRatios = ['1:1', '3:2', '2:3', '16:9', '9:16'] as const;
 
 export type Ratio = (typeof availableRatios)[number];
-
-export type DocumentationWrapperVariants = NonNullable<
-  RecipeVariants<typeof documentationWrapper>
->;
